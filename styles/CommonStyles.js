2341cb4e3eda29aa8cf45abab9893df3f0c9a115
Josefine-Ha <josefine.hansson@ha.ax>
2025-03-29 17:12:32 +0200

    Lagt till checkar för namn och negativa värden

--- styles/CommonStyles.js
+++ styles/CommonStyles.js
@@ -2,16 +2,30 @@ import { StyleSheet } from "react-native";
 
 export const CommonStyles = StyleSheet.create({
   textItem: {
-    fontSize: 40,
+    fontSize: 30,
+    fontWeight: "bold",
+    flex: 1,
+    marginRight: 'auto',
+  },
+  textItemSmall: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
   row: {
+    borderWidth: 3,
+    borderColor: "lightblue",
     flexDirection: "row",
     alignItems: "center",
-    borderColor: "lightblue",
-    borderWidth: 3,
-    borderRadius: 5,
-    margin: 5,
+    marginBottom: 5,
+    padding: 5,
     paddingEnd: 20,
     paddingStart: 20,
+  },
+  input: {
+    flex: 1,                     // Gör textfältet flexibelt och låter det ta upp maxutrymme
+    height: 50,                  // Höjd på textfältet
+    paddingHorizontal: 10,       // Utrymme inuti fältet
+    marginRight: 10,             // Liten avstånd till knappen 
+    fontSize: 20,              // Textstorlek
   },
 });
